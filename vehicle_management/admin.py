a656c6276aaefa01df139a88e64956907320ed39
Goumes <alejandro_golivera@outlook.com>
2024-01-30 14:48:52 +0100

    Arreglada la lista

--- vehicle_management/admin.py
+++ vehicle_management/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from vehicle_management.models import Vehiculo
+from vehicle_management.models import Vehiculo, Marca
 
 # Register your models here.
 admin.site.register(Vehiculo)
+admin.site.register(Marca)
--- vehicle_management/views.py
+++ vehicle_management/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, generics
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from vehicle_management.models import Vehiculo, Marca
 from vehicle_management.serializers import GroupSerializer, UserSerializer, VehiculoSerializer, MarcaSerializer
@@ -27,6 +29,20 @@ class VehiculoViewSet(viewsets.ModelViewSet):
     queryset = Vehiculo.objects.all()
     serializer_class = VehiculoSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=False, methods=['GET'])
+    def lista_por_marca(self, request):
+        marca = Marca.objects.all().first()
+        vehiculos = self.queryset.filter(marca__nombre=marca.nombre)
+        #serializer = self.get_serializer(data=vehiculos, many=True)
+        serializer = self.serializer_class(vehiculos, many=True, context={'request': self.request})
+        response = Response(serializer.data)
+        #if serializer.is_valid():
+        #    response = Response(serializer.validated_data)
+        #else:
+        #    print(serializer.errors)
+        #    response = Response({"No tira": "unclucky"})
+        return response
 
 
 class MarcaViewSet(viewsets.ModelViewSet):
